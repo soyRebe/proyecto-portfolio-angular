dfa7dcf362741b9bcd3fdbf67479b245be2c7d43
rechegoy <rebeca.echegoy@gmail.com>
2021-04-06 18:23:39 -0300

    Agregado de servicio para listar proyectos

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -27,4 +27,11 @@ export class ProjectService {
 
         return this._http.post( this.url+'save-project', params, {headers: headers} );
     }
+
+    getProjects(): Observable<any> {
+
+        let headers = new HttpHeaders().set('Content-Type', 'application/json'); // les paso las cabeceras con las que voy a enviar los datos
+        return this._http.get( this.url+'projects',{headers: headers} );
+
+    }
 }
